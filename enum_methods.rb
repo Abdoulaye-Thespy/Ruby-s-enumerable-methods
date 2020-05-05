5cacc28f13fb4514b643edbecf3634820d5a2445
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-05-05 09:55:22 +0100

    added my_select

--- enum_methods.rb
+++ enum_methods.rb
@@ -1,29 +1,45 @@
-module enumerable 
+module Enumerable 
   def my_each
   	return to_enum :my_each unless block_given?
   	for i in self
   		yield(i)
   	end
-end
+  end
 
   def my_each_with_index
-  	return to_enum :my_each unless block_given?
+  	return to_enum :my_each_with_index unless block_given?
   	i=0
   	for element in self
   		yield(element, i)
   		i+=1
     end
+  end
+
+
+  def my_select
+    return to_enum :my_select unless block_given?
+    new_arr=[]
+    for i in self
+         if yield(i) == true
+         new_arr << i
+       end
+    end
+     p new_arr
+  end
+
+  def my_all
+    return true unless block_given?
+    
+  end
+
 end
-
-
-
-
-
 
 
 
 
 puts "**********************************************************************************************************"
 
-myarray=[20,30,40,5]
+myarray=[20,30,40,5,400,401,22,25,53]
 myarray.my_each
+myarray.my_each_with_index
+myarray.my_select
