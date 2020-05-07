6f160fc1d313d298ef9b025c6b87adbc7eb5d15a
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-05-07 09:25:14 +0100

    added my_inject's multiply_els

--- enum_methods.rb
+++ enum_methods.rb
@@ -1,109 +1,126 @@
 module Enumerable
   def my_each
     return self unless block_given?
-  	for i in self
-  		yield(i)
-  	end
 
+    each do |i|
+      yield(i)
+    end
   end
 
   def my_each_with_index
-  	return self unless block_given?
-  	i=0
-  	for element in self
-  		yield(element, i)
-  		i+=1
+    return self unless block_given?
+
+    i = 0
+    each do |element|
+      yield(element, i)
+      i += 1
     end
   end
 
-
   def my_select
     return self unless block_given?
-    new_arr=[]
-    l=self.length
+
+    new_arr = []
+    l = length
     l.times do |i|
-        new_arr << self[i] if yield(self[i])
-        i += 1
+      new_arr << self[i] if yield(self[i])
+      i += 1
     end
-      new_arr
+    new_arr
   end
 
   def my_all?
     return true unless block_given?
-    for i in self
-      t_f= yield(i)
+
+    each do |i|
+      t_f = yield(i)
       print t_f
       return false unless t_f
     end
-    return true
+    true
   end
 
   def my_any?
     return true unless block_given?
-    for i in self
-      t_f=yield(i)
+
+    each do |i|
+      t_f = yield(i)
       return true if t_f
     end
-  return false
+    false
   end
 
   def my_none?
     return true unless block_given?
-    for i in self
-      t_f=yield(i)
+
+    each do |i|
+      t_f = yield(i)
       return false if t_f
     end
-  return true
+    true
   end
 
   def my_count
     return self unless block_given?
-    count=0
-    l=self.length
+
+    count = 0
+    l = length
     l.times do |i|
-        count+=1 if yield(self[i])
-         i += 1
+      count += 1 if yield(self[i])
+      i += 1
     end
-      count
+    count
   end
 
-    def my_map ()
+  def my_map()
+    puts self
     return self unless block_given?
-    new_arr=[]
-    l=self.length
-    l.times do |i|
-        new_arr << yield(self[i])
-        i += 1
-    end
-      p new_arr
-  end
 
-    def my_inject (base)
+    new_arr = []
+    l = length
+    l.times do |i|
+      new_arr << yield(self[i])
+      i += 1
+    end
+
+    p new_arr
+end
+
+  def my_inject(base)
     return self unless block_given?
-    sum=base
-    l=self.length
+
+    sum = base
+    l = length
     l.times do |i|
-        sum= yield(sum, self[i])
-        puts sum
-        i += 1
+      sum = yield(sum, self[i])
+      i += 1
     end
-      sum
+    sum
+end
+
+  def multiply_els(array)
+    r = array.my_inject(1) do |x, y|
+      x * y
+    end
+    p r
+    r
   end
 
+  puts '**********************************************************************************************************'
 
-puts "**********************************************************************************************************"
-
-myarray=[20,30,40,5,400,401,22,25,53, 10, 12, 12, 454545]
-myhash={"1" => "January", "2" => "February"}
-myarray.my_each 
-myhash.my_each
-myarray.my_each_with_index
-r=myarray.my_select
-myhash.my_select
-myarray.my_all?
-myarray.my_any?
-myarray.my_none?
-myarray.my_count
-myarray.my_map
-r=myarray.my_inject
+  myarray = [20, 30, 40, 5, 400, 401, 22, 25, 53, 10, 12, 12, 454_545]
+  myhash = { '1' => 'January', '2' => 'February' }
+  myarray.my_each
+  myhash.my_each
+  myarray.my_each_with_index
+  r = myarray.my_select
+  myhash.my_select
+  myarray.my_all?
+  myarray.my_any?
+  myarray.my_none?
+  myarray.my_count
+  myarray.my_map
+  myarray.my_inject(0)
+  r = myarray.multiply_els([2, 3, 4, 5])
+  p r
 end
