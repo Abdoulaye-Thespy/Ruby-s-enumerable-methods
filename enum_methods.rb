784a094cb52f9a17ff34d73ce43c89d87f25ba9d
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-05-08 10:49:20 +0100

    fixed rubocop errors and made some changes on the methods

--- enum_methods.rb
+++ enum_methods.rb
@@ -1,6 +1,6 @@
 module Enumerable
   def my_each
-    return to_enum: my_each unless block_given?
+    return to_enum(:my_each) unless block_given?
 
     each do |i|
       yield(i)
@@ -8,22 +8,21 @@ module Enumerable
   end
 
   def my_each_with_index
-    return to_enum: my_each_with_index unless block_given?
+    return to_enum(:my_each_with_index) unless block_given?
 
     i = 0
-    each do |element|
+    my_each do |element|
       yield(element, i)
       i += 1
     end
   end
 
   def my_select
-    return to_enum: my_select unless block_given?
+    return to_enum(:my_select) unless block_given?
 
     new_arr = []
-    l = length
-    l.times do |i|
-      new_arr << self[i] if yield(self[i])
+    my_each do |i|
+      new_arr << i if yield(i)
     end
     new_arr
   end
@@ -92,7 +91,7 @@ module Enumerable
 
     unless block_given?
 
-      my_each { |ind| return false if ind == false || ind.nil? }
+      my_each { |ind| return false if ind !=false || !ind.nil? }
       return true
     end
 
@@ -104,21 +103,23 @@ module Enumerable
   end
 
   def my_count(arg = nil)
-    return self unless block_given?
 
     count = 0
-    l = length
 
     unless arg.nil?
       if arg.is_a?(Regexp)
-        my_each { |ind| count += 1 if ind.match(arg) }
+        my_each { |ind| count += 1  if ind.match(arg) }
+      elsif arg.is_a?(Numeric)
+        my_each {|ind| count +=1 if ind===arg}
+          
 
       end
+      return count
     end
-    return l unless block_given
+    return self.length unless block_given?
 
-    l.times do |i|
-      count += 1 if yield(self[i])
+    my_each do |i|
+      count += 1 if yield(i)
     end
     count
   end
@@ -126,7 +127,7 @@ module Enumerable
   def my_map(arg = nil)
     new_arr = []
     my_each { |num| arr << arg.call(num) } if arg.is_a?(Proc)
-    return to_enum: my_each_map unless block_given?
+    return to_enum(:my_map) unless block_given?
 
     my_each do |i|
       new_arr << yield(i)
@@ -134,7 +135,7 @@ module Enumerable
   end
 
   def my_inject(base = nil)
-    return to_enum: my_inject unless block_given?
+    return to_enum(:my_inject) unless block_given?
 
     sum = base
     l = length
@@ -150,20 +151,4 @@ module Enumerable
     end
     r
   end
-
-  puts '**********************************************************************************************************'
-
-  myarray = [20, 30, 40, 5, 400, 401, 22, 25, 53, 10, 12, 12, 454_545]
-  myhash = { '1' => 'January', '2' => 'February' }
-  myarray.my_each
-  myhash.my_each
-  myarray.my_each_with_index
-  myarray.my_select
-  myhash.my_select
-  myarray.my_all?
-  myarray.my_any?
-  myarray.my_none?
-  myarray.my_count
-  myarray.my_map
-  myarray.my_inject
-end
+ end
